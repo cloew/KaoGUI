8f5bbcab1957128075136b6de8312900bb9f299b
Chris Loew <cloew123@gmail.com>
2013-08-22 20:49:04 -0600

    Added more than a little stubbed out controller class

---
+++ kao_gui/controller.py
@@ -0,0 +1,49 @@
+
+class Controller:
+    """ Represents a UI Controller """
+    
+    def __init__(self, screen, commands={}):
+        """ Initialize the controller """
+        self.screen = screen
+        self.commands = commands+{ESCAPE:self.stopRunning}
+        self.running = False
+    
+    def run(self):
+        """  """
+        self.running = True
+        
+        while self.isRunning():
+            self.performGameCycle()
+            
+            if self.isRunning():
+                self.handleInput()
+                
+                window = self.getWindow()
+                window.setScreen(self.screen)
+                self.window.draw()
+                
+    def getWindow(self):
+        """ Returns the Window for the Controller to interact with """
+        
+    def getInputProcessor(self):
+        """ Returns the Input Processor for the controller """
+            
+    def stopRunning(self):
+        """ Stop running this controller """
+        self.running = False
+        
+    def isRunning(self):
+        """ Returns if the controller is still running """
+        return self.running
+        
+    def performGameCycle(self):
+        """ Perform a Game Cycle Event """
+        
+    def handleInput(self):
+        """ Handle User Input """
+        self.getInputProcessor().processInput(self.commands)
+        
+    def addCommand(self, character, command):
+        """ Add the given command to happen on the given character """
+        self.commands[character] = command
+        
