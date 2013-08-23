976e31da3ce17c63aab79bbfd178b92f1af2e6e9
Chris Loew <cloew123@gmail.com>
2013-08-22 21:55:19 -0600

    Added stubbed out Window and Input Processor

--- kao_gui/console/console_controller.py
+++ kao_gui/console/console_controller.py
@@ -1,12 +1,14 @@
 from kao_gui.controller import Controller
+from kao_gui.console.input_processor import InputProcessor
+from kao_gui.console.window import Window
 
 class ConsoleController(Controller):
     """ Represents a Controller for the Console UI """
         
     def getWindow(self):
         """ Return the Console Window """
-        
+        return Window
         
     def getInputProcessor(self):
         """ Return Input Processor """
-        
+        return InputProcessor
---
+++ kao_gui/console/window.py
@@ -0,0 +1,54 @@
+from blessings import Terminal
+
+class ConsoleWindow(object):
+    """ Represents the Console Window displayed to the user """
+    
+    def __init__(self):
+        """ Build the window """
+        self.screen = None
+        self.terminal = Terminal()
+        
+        print self.terminal.enter_fullscreen()
+        print self.terminal.hide_cursor()
+        
+    def close(self):
+        print self.terminal.exit_fullscreen()
+        print self.terminal.normal_cursor()
+
+    @property
+    def width(self):
+        """ Return the width of the window """
+        return self.terminal.width
+    
+    @property
+    def height(self):
+        """ Return the height of the window """
+        return self.terminal.height
+        
+    def setScreen(self, screen):
+        """ Sets the current Screen Display """
+        self.screen = screen
+        window.clear()
+        
+    def update(self):
+        """ Update the screen """
+        self.screen.update()
+        self.screen.draw(self)
+        
+    def draw(self):
+        """ Draw the Screen """
+        self.screen.draw()
+        
+    # def draw(self, text, pos):
+        # """ Draws the text to the window """
+        # lineNum = pos[1]
+        # for line in text:
+            # with self.terminal.location(int(pos[0]), int(lineNum)):
+                # print line
+            # lineNum += 1
+        
+    def clear(self):
+        """ Clears the window """
+        print self.terminal.clear()
+        
+Window = ConsoleWindow()
