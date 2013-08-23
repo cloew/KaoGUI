542500d3e7b4c6d450c8ac4860459d181d346eb5
Chris Loew <cloew123@gmail.com>
2013-08-22 21:23:54 -0600

    Added Console Controller

---
+++ kao_gui/console/console_controller.py
@@ -0,0 +1,12 @@
+from kao_gui.controller import Controller
+
+class ConsoleController(Controller):
+    """ Represents a Controller for the Console UI """
+        
+    def getWindow(self):
+        """ Return the Console Window """
+        
+        
+    def getInputProcessor(self):
+        """ Return Input Processor """
+        
