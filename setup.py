bb2a5977090f4acdbb4f957284c87bb8c7f33df2
Chris Loew <cloew123@gmail.com>
2013-08-22 21:55:34 -0600

    Updated setup file to install the console package

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='kao_gui',
       author='Chris Loew',
       author_email='cloew123@gmail.com',
       #url='http://www.python.org/sigs/distutils-sig/',
-      packages=['kao_gui'],
+      packages=['kao_gui', 'kao_gui/console'],
      )
