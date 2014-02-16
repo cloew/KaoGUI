c616c9e662c1a51493c04ea5a6ebec672221d336
Chris Loew <cloew123@gmail.com>
2014-02-16 14:36:09 -0700

    Updated setup script to install kao_gui pygame

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(name='kao_gui',
-      version='0.2',
+      version='0.3',
       #description='Kao Tessur Cygwin Console Input Processor',
       author='Chris Loew',
       author_email='cloew123@gmail.com',
       #url='http://www.python.org/sigs/distutils-sig/',
-      packages=['kao_gui', 'kao_gui/console'],
+      packages=['kao_gui', 'kao_gui/console', 'kao_gui/pygame'],
      )
