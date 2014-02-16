cd30c1f5114bc2e2250352617aea25f844d35ece
Chris Loew <cloew123@gmail.com>
2014-02-16 14:08:37 -0700

    Added the pygame specific input processor

---
+++ kao_gui/pygame/input_processor.py
@@ -0,0 +1,41 @@
+
+class InputProcessor:
+    """ Class to process input from Pygane and convert them to game commands """
+    
+    def __init__(self):
+        """ Builds the input processor """
+        self.bindings = None
+        
+    def processInputs(self, functions):
+        """ Process inputs to functions """
+        commands = self.convertEventToCommand()
+        
+        for command in commands:
+            if command in functions:
+                functions[command]()
+            elif command[0] in functions and command[1] is PRESSED:
+                functions[command[0]]()
+        
+    def convertEventToCommand(self):
+        """ Converts PyGame Events to Game Commands """
+        commands = []
+        
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pass # Should add some sort of wrapper to the Quit Event here
+            elif event.type == KEYUP:
+                self.addKeyEvent(commands, event, RELEASED)
+            elif event.type == KEYDOWN:
+                self.addKeyEvent(commands, event, PRESSED)
+        
+        return commands
+        
+    def addKeyEvent(self, commands, event, keyState):
+        """ Adds a Key Event to the Commands list """
+        if self.bindings is None:
+            commands.append((event.key, keyState))
+        elif event.key in self.bindings.keyBindings:
+            gameAction = self.bindings.keyBindings[event.key]
+            commands.append((gameAction, keyState))
+        
+inputProcessor = InputProcessor()
