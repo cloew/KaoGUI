75a6da4f5dd0cbb8ff4dee46a42611ba813efdd1
Chris Loew <cloew123@gmail.com>
2014-02-16 16:58:09 -0700

    Added Icon Builder helper class

---
+++ kao_gui/pygame/icon_builder.py
@@ -0,0 +1,23 @@
+
+class IconBuilder:
+    """ Class to aid in construction of valid Icons """
+    
+    def __init__(self, iconFilename):
+        """ Initialie the Builder with the filename to load from """
+        self.iconFilename = iconFilename
+        
+    def buildIcon(self):
+        """ Build and return the Icon Surface """
+        iconSurface = pygame.Surface((32,32))
+        rawIconSurface = load_image(self.iconFilename)
+        self.setTransaprency(iconSurface, rawIconSurface)
+        
+        return iconSurface
+        
+    def setTransparency(self, iconSurface, rawIcon):
+        """ Set the transparency on the icon surface """
+        iconSurface.set_colorkey(rawIcon.get_at((0,0)))
+        
+        for i in range(0,32):
+            for j in range(0,32):
+                iconSurface.set_at((i,j), rawIcon.get_at((i,j)))
