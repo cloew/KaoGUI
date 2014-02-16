76c86219cdc32106a6b739260bce166d0c836dd1
Chris Loew <cloew123@gmail.com>
2014-02-16 14:26:43 -0700

    Added the pygame window and a standard helper file

---
+++ kao_gui/pygame/pygame_helper.py
@@ -0,0 +1,19 @@
+import pygame
+from pygame.locals import *
+
+def load_image(imageFilename, colorkey=None):
+    try:
+        image = pygame.image.load(imageFilename)
+    except pygame.error, message:
+        print 'Cannot load image:', name
+        raise SystemExit, message
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0,0))
+        image.set_colorkey(colorkey, RLEACCEL)
+    return image
+    
+def GetTransparentSurface(width, height):
+    """ Constructs a transparent pygame surface """
+    surface = pygame.Surface((width, height), flags=pygame.SRCALPHA)
+    return surface
---
+++ kao_gui/pygame/window.py
@@ -0,0 +1,64 @@
+import pygame
+
+from kao_gui.pygame.pygame_helper import load_image
+from pygame.locals import *
+
+class PygameWindow:
+    """ Represents the Pygame window displayed to the user """
+    GAME_SPEED = 60
+    
+    def __init__(self, width=640, height=480, caption='Pokemon', iconFilename='resources/images/pokeball3.bmp'):
+        """ Build the window """
+        self.window = self.getWindow(caption, iconFilename)
+        self.clock = pygame.time.Clock()
+        
+        self.width = width
+        self.height = height
+        
+        self.screen = None
+        
+    def setScreen(self, screen):
+        """ Sets the current Screen Display """
+        self.screen = screen
+        
+    def getWindow(self, caption, iconFilename):
+        """ Gets the GUI Window """
+        pygame.init()
+        pygame.display.set_icon(self.getIcon(iconFilename))
+        pygame.display.set_caption(caption)
+        pygame.mouse.set_visible(0)
+        return pygame.display.set_mode((self.width, self.height))
+        
+    def getIcon(self, iconFilename):
+        """ Gets the icon """
+        icon = pygame.Surface((32,32))
+        rawicon = load_image(iconFilename) # must be 32x32, black is transparant
+        icon.set_colorkey(rawicon.get_at((0,0)))
+        
+        for i in range(0,32):
+            for j in range(0,32):
+                icon.set_at((i,j), rawicon.get_at((i,j)))
+        return icon
+        
+    def update(self):
+        """ Update the screen """
+        self.clock.tick(self.GAME_SPEED)
+        self.screen.update()
+        self.screen.draw(self)
+        self.redraw()
+        
+    def draw(self, surface, pos):
+        """ Draws the surface to the window """
+        self.window.blit(surface, pos)
+            
+    def redraw(self):
+        pygame.display.flip()
+        
+    def clear(self):
+        """ Clears the window """
+        self.window.fill((255,255,255))
+        
+    def close(self):
+        """ Close the window...? """
+   
+Window = PygameWindow()
