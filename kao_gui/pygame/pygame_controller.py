fb230ee88e438cb9d42729d06d13a26e1a9deecf
Chris Loew <cloew123@gmail.com>
2014-02-16 20:44:12 -0700

    Updated Pygame Controller to inherit from the Base Controller class

--- kao_gui/pygame/pygame_controller.py
+++ kao_gui/pygame/pygame_controller.py
@@ -1,9 +1,10 @@
+from kao_gui.controller import Controller
 from kao_gui.pygame.input_processor import InputProcessor
 from kao_gui.pygame.quit_event_wrapper import QuitEventWrapper as QuitEvent
 from kao_gui.pygame.window import Window
 
 
-class PygameController:
+class PygameController(Controller):
     """ Represents a Controller for the Pygame UI """
     
     def __init__(self, screen, commands=None, cancellable=False):
@@ -11,6 +12,7 @@ class PygameController:
         if commands is None:
             commands = {}
         commands[QuitEvent] = self.quitToDesktop
+        Controller.__init__(self, screen, commands=commands)
         
     def getWindow(self):
         """ Return the Console Window """
