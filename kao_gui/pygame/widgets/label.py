f7bdd4c55ffa0d338452adce7256c4796b07acae
Chris Loew <cloew123@gmail.com>
2014-04-12 12:26:08 -0600

    Can now specify the color of a label

--- kao_gui/pygame/widgets/label.py
+++ kao_gui/pygame/widgets/label.py
@@ -5,15 +5,16 @@ import pygame
 class Label(PygameWidget):
     """ Represents a text label displayed via Pygame """
     
-    def __init__(self, text, font="Times New Roman", size=36, bold=False):
+    def __init__(self, text, font="Times New Roman", size=36, bold=False, color=(10, 10, 10)):
         """ Initialize the Image by loading the Image File given """
         self.text = text
         self.font = pygame.font.SysFont(font, size)
+        self.color = color
         self.setBold(bold)
         
     def buildSurface(self):
         """ Build the surface to display """
-        return self.font.render(self.text, 1, (10, 10, 10))
+        return self.font.render(self.text, 1, self.color)
         
     def setBold(self, bold):
         """ Sets the Boldness of the entry """
