854071716adcf1c7efe7bffc606bd3bad6a23c36
Chris Loew <cloew123@gmail.com>
2014-06-23 16:20:43 -0600

    Image can now be given a set scaled size

--- kao_gui/pygame/widgets/image.py
+++ kao_gui/pygame/widgets/image.py
@@ -1,12 +1,16 @@
 from kao_gui.pygame.pygame_helper import load_image
 from kao_gui.pygame.pygame_widget import PygameWidget
 
+import pygame
+
 class Image(PygameWidget):
     """ Represents an image displayed via Pygame """
     
-    def __init__(self, imageFilename):
+    def __init__(self, imageFilename, scaledSize=None):
         """ Initialize the Image by loading the Image File given """
         self.image = load_image(imageFilename)
+        if scaledSize is not None:
+            self.image = pygame.transform.scale(self.image, scaledSize)
         
     def buildSurface(self):
         """ Build the surface to display """
